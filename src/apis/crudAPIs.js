537223e0841508545d636c203d8c5a4d8ebbf03d
Md ibrahim adham mesu <mesu.s.ss@gmail.com>
2023-01-23 18:46:04 +0600

    Add data

--- src/apis/crudAPIs.js
+++ src/apis/crudAPIs.js
@@ -17,8 +17,28 @@ const crudAPIs={
 
   },
   async deleteData(id){
-    console.log(id)
+    // console.log(id)
     const response= await api.delete(`/posts/${id}`);
+    // console.log(response);
+    if([200,201].includes(response.status)){
+
+    }
+    return response;
+
+  },
+  async updateData(data){
+    // console.log(data)
+    const response= await api.put(`/posts/${data?.id}`,data);
+    // console.log(response);
+    if([200,201].includes(response.status)){
+
+    }
+    return response;
+
+  },
+  async addData(data){
+    // console.log(data)
+    const response= await api.post('/posts',data);
     // console.log(response);
     if([200,201].includes(response.status)){
 
@@ -30,6 +50,6 @@ const crudAPIs={
 
 }
 
-export const {getData,deleteData}=crudAPIs;
+export const {getData,deleteData,updateData,addData}=crudAPIs;
 
 export default crudAPIs;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,28 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { deleteData, getData } from '../apis/crudAPIs';
+import { addData, deleteData, getData, updateData } from '../apis/crudAPIs';
 
 const LandingPage = () => {
-  const [postData, setPostData] = useState([]);
-  // const queryClient = useQueryClient();
-  const { data } = useQuery(['get-Data'], () => getData());
-  const { mutateAsync } = useMutation(['delete-Data'], (data) => deleteData(data),
+  // const [postData, setPostData] = useState([]);
+  const queryClient = useQueryClient();
+  const { data } = useQuery(['get-data'], () => getData());
+  const { mutateAsync:deleteAsyncDate } = useMutation(['delete-data'], (data) => deleteData(data),
     // {
     //   onSuccess: () => queryClient.invalidateQueries(['get-Data']),
     // }
   );
 
+  const {mutateAsync:updateAsyncData}=useMutation(['update-data'],(data)=>updateData(data))
+  const {mutateAsync:addAsyncData}=useMutation(['add-data'],(data)=>addData(data))
+
   const handleDelete = async (id) => {
-    setPostData(postData.filter((item) => item.id !== id));
-    const response = await mutateAsync(id);
+    // setPostData(postData.filter((item) => item.id !== id));
+    const response = await deleteAsyncDate(id);
     console.log(response);
   };
 
-  // console.log(data);
+  const handleUpdate = async(id)=>{
+    const updateData={
+      id: id,
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }
+    
+    const response=await updateAsyncData(updateData);
+    console.log(response)
 
-  useEffect(() => {
-    setPostData(data);
-  }, [data]);
+  }
+
+  const handleAdd= async()=>{
+    const addData={
+
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }
+    const response=await addAsyncData(addData);
+    console.log(response);
+  }
+
+  // useEffect(() => {
+  //   setPostData(data);
+  // }, [data]);
 
   return (
     <div
@@ -30,8 +55,10 @@ const LandingPage = () => {
       style={{ margin: '0 50px' }}
     >
       <h4>Data Table</h4>
+
       <hr />
-      {postData?.map((item) => (
+      <button onClick={()=>handleAdd()} type='button'>Add New</button>
+      {data?.map((item) => (
         <div
           key={item?.id}
           style={{
@@ -43,7 +70,7 @@ const LandingPage = () => {
           className=''
         >
           <p>{item?.title}</p>
-          <button type='button'>Update</button>
+          <button onClick={()=>handleUpdate(item?.id)} type='button'>Update</button>
           <button
             onClick={() => handleDelete(item?.id)}
             type='button'
